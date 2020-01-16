b8889f6425c480925307af865099c03490d71808
Oukache Ayas <ayasouk.iam@gmail.com>
2020-01-16 07:54:16 +0100

    DFA for anbn language

--- main.c
+++ main.c
@@ -3,78 +3,61 @@
 #include <string.h>
 #define _SUCCESS 1
 #define _FAIL 0
+ 
+ int bA(char st[10], int *deb, int *fin){
+     char c = 'a';
+     if(st[(*deb)]==c){
+         (*deb)++;
+         return _SUCCESS;
+     }
+     else {
+         return _FAIL;
+     }
+ }
+  
+int bB(char st[10],int *deb, int *fin){
+     char c = 'b';
 
-typedef struct point {
-    int x;
-    int y;
-} Point ;
-
-int bA(char ** st, int deb, int fin){
-    if ((*st[deb])=="a"){
-        deb++;
-        return _SUCCESS;
-    }
-    else{
-        return _FAIL;
-    }
+     if(st[(*fin)]==c){
+     	(*fin)--;
+	return _SUCCESS;
+     }
+     else {
+     	return _FAIL;
+     }
 }
 
-int bB(char ** st, int deb, int fin){
-    if((*st[deb])=="b"){
-        deb++;
-        return _SUCCESS;
-    }
-    else{
-        return _FAIL;
-    }
+int bS(char st[10], int *deb, int *fin){
+     if((*deb)>(*fin)){
+     	return _SUCCESS;
+     }
+     else if(bA(st,deb,fin)&&bB(st,deb,fin)&&bS(st,deb,fin)){
+	return _SUCCESS;
+     }
+     else if(bA(st,deb,fin)&&bB(st,deb,fin)){
+     	return _SUCCESS;
+     }
+     else{
+     	return _FAIL;
+     }
+
 }
 
-int bS(char ** st, int deb, int fin){
-    if((*st)=="ab"){
-        return _SUCCESS;
-    }
-    else if((*st)[deb]=="a" & (*st)[fin]=="b" & bS(st, deb++, fin--)){
-        return _SUCCESS;
-    }
-    else{
-        return _FAIL;
-    }
-}
-
-int stringSize(char* c){
-    int i =0;
-    while(c[i]!="\0"){
-        i++;
-    }
-    return i+1;
-}
-
-int main()
-{
-    Point p;
-    int i;
-    p.x=1;
-    p.y=2;
-    char st[3][10];
-    strcpy(st[0],"ab");
-    strcpy(st[1],"aaabbb");
-    strcpy(st[2],"aabaaba");
-
-    printf(st[0]);
-    if(bA("a",0,2)){
-        printf("it works");
-    }
-    else{
-        printf("nop");
-    }
-    /*for(int i=0; i<3; i++){
-        printf("current word : %s ", st[i]);
-        if(bS(&st[i], 0, stringSize(st[i]))){
-            printf("SUCCESS");
-        }
-        else{
-            printf("FAIL");
-        }
-    }*/
-    return _SUCCESS;
+ int main()
+ {   
+     int i=0;
+     int j;
+     char st[10]="aaabebb";
+     j = strlen(st)-1;
+     printf("ind deb : %d\n",i);
+     printf("ind fin : %d\n",j);
+     if(bS(st,&i,&j)){
+     	printf("%s is a part of the language\n",st);
+     }
+     else{
+     	printf("%s is not a part of the language\n",st);
+     }
+     printf("ind deb : %d\n",i);
+     printf("ind fin : %d\n",j);
+     return _SUCCESS;
 }
